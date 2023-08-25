f46e95f866960cc73e696afe51314a07d9c3b096
Janek1010 <janekdomozych99@gmail.com>
2023-08-25 14:47:18 +0200

    Stop gninnipS My sdroW! solution

--- SumOfPositive/src/main/java/org/example/Main.java
+++ SumOfPositive/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Arrays;
+
+/***You get an array of numbers, return the sum of all of the positives ones.
+
+ Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+ Note: if there is nothing to sum, the sum is default to 0.
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int arr[] = {1,-4,7,12};
+        System.out.println(sum(arr));
+    }
+    public static int sum(int[] arr){
+        return Arrays.stream(arr).filter(p -> p > 0).sum();
     }
 }
